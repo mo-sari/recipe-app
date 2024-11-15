8a142f1dea661fef528cfd63808728c160929808
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-11-15 21:03:56 +0330

    added test and functionality of adding superuser

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -34,3 +34,13 @@ class ModelTests(TestCase):
         """Test if user has provided any emails"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'smaple123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            email='test@example.com',
+            password='test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
